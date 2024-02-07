ae7aa9b8f74c2a4b02ebe724c098c4d75c9e5bab
Leonardo Santos <92065373+Sleozin@users.noreply.github.com>
2024-02-07 17:04:03 -0300

    Reconhecendo símbolos

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -15,7 +15,7 @@ public class App
     {
 
     
-        String input = "45 variavel while if";
+        String input = "45 variavel + while < , if";
         Scanner scan = new Scanner (input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
             System.out.println(tk);
