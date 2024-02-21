0ed66b09eaa8f572954ad0d952f25d38f5bce6b5
Leonardo Santos <92065373+Sleozin@users.noreply.github.com>
2024-02-21 19:07:26 -0300

    Geração de Código Intermediário - Comando return

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -247,8 +247,11 @@ public class Parser {
         expectPeek(RETURN);
         if (!peekTokenIs(SEMICOLON)) {
             parseExpression();
+        }else{
+            vmWriter.writePush(Segment.CONST, 0);
         }
         expectPeek(SEMICOLON);
+        vmWriter.writeReturn();
 
         printNonTerminal("/returnStatement");
     }
