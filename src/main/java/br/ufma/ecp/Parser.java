74a74e8d9707602a0183da890e4a75336cac2cd3
Leonardo Santos <92065373+Sleozin@users.noreply.github.com>
2024-02-21 18:44:25 -0300

    Geração de Código Intermediário - Literais keyword

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -304,8 +304,14 @@ public class Parser {
             case FALSE:
             case NULL:
             case TRUE:
+                expectPeek(FALSE, NULL, TRUE);
+                vmWriter.writePush(Segment.CONST, 0);
+                if (currentToken.type == TRUE)
+                    vmWriter.writeArithmetic(Command.NOT);
+                break;
             case THIS:
-                expectPeek(FALSE, NULL, TRUE, THIS);
+                expectPeek(THIS);
+                vmWriter.writePush(Segment.POINTER, 0);
                 break;
             case IDENT:
                 expectPeek(IDENT);
