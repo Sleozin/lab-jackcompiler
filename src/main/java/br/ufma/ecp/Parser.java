c669d1a0fee9e4e395055de1fb023544f4d31900
Leonardo Santos <92065373+Sleozin@users.noreply.github.com>
2024-02-21 23:03:12 -0300

    Geração de Código Intermediário - Chamada de procedimentos (comando do)

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -108,6 +108,7 @@ public class Parser {
         expectPeek(IDENT);
         parseSubroutineCall();
         expectPeek(SEMICOLON);
+        vmWriter.writePop(Segment.TEMP, 0);
 
         printNonTerminal("/doStatement");
     }
