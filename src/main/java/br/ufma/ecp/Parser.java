958076d7d4e27af098944aa9b1918018a5eadb18
Leonardo Santos <92065373+Sleozin@users.noreply.github.com>
2024-02-21 22:42:18 -0300

    Geração de Código Intermediário - Implementando arrays

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -230,6 +230,8 @@ public class Parser {
         if (peekTokenIs(LBRACKET)) {
             expectPeek(LBRACKET);
             parseExpression();
+            vmWriter.writePush(kind2Segment(symbol.kind()), symbol.index());
+            vmWriter.writeArithmetic(Command.ADD);
             expectPeek(RBRACKET);
             isArray = true;
         }
@@ -237,8 +239,10 @@ public class Parser {
         expectPeek(EQ);
         parseExpression();
         if (isArray) {
-    
-
+            vmWriter.writePop(Segment.TEMP, 0);    // push result back onto stack
+            vmWriter.writePop(Segment.POINTER, 1); // pop address pointer into pointer 1
+            vmWriter.writePush(Segment.TEMP, 0);   // push result back onto stack
+            vmWriter.writePop(Segment.THAT, 0);    // Store right hand side evaluation in THAT 0.
         } else {
             vmWriter.writePop(kind2Segment(symbol.kind()), symbol.index());
         }
@@ -432,7 +436,11 @@ public class Parser {
                     if (peekTokenIs(LBRACKET)) { // array
                         expectPeek(LBRACKET);
                         parseExpression();
+                        vmWriter.writePush(kind2Segment(sym.kind()), sym.index());
+                        vmWriter.writeArithmetic(Command.ADD);
                         expectPeek(RBRACKET);
+                        vmWriter.writePop(Segment.POINTER, 1); // pop address pointer into pointer 1
+                        vmWriter.writePush(Segment.THAT, 0);   // push the value of the address pointer back onto stack
                     } else {
                         vmWriter.writePush(kind2Segment(sym.kind()), sym.index());
                     }
